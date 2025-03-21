0f7ca93eb5ecf2929d362b4982a50f3d9a05f03a
reAlpha39 <yunusw718@gmail.com>
2025-03-21 11:07:04 +0700

    feat: update icon and product name

--- ClientPrinterApp/Properties/AssemblyInfo.cs
+++ ClientPrinterApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClientPrinterApp")]
+[assembly: AssemblyTitle("PrinterServiceHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClientPrinterApp")]
+[assembly: AssemblyProduct("PrinterServiceHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
